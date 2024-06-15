79118309b5b6f5d4d857e552bc635c4b308a82c5
Akanksha Raghav <akanksharaghav919@gmail.com>
2024-06-15 11:58:55 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,20 @@
+// Product.java
+package com.example.productservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    // other fields, getters and setters
+}
