eeb55c1510c9db3d07c45428704532be825a2c27
Akanksha Raghav <akanksharaghav919@gmail.com>
2024-06-17 11:46:11 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,21 @@
+// Task.java
+package com.example.taskservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String status;
+
+    // Getters and Setters
+}
